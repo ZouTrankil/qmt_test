5d755c1b5e638ad8f2922d89a96b4832652032ad
邹强 <515600147@qq.com>
2025-03-10 09:40:41 +0800

    3

---
+++ 资金跟踪/global_config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 10/03/2025.
+@author: Air.Zou
+"""
+import tushare as ts
+ts.set_token('2876ea85cb005fb5fa17c809a98174f2d5aae8b1f830110a5ead6211')
+pro = ts.pro_api()
+
